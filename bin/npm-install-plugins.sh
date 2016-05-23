b4ac7b423c0c1ddbfa85e399514a8715d9a387e1
Sergei Vizel <sergei.vizel@modera.net>
2016-05-23 14:59:48 +0300

    install-plugins: follow symbolic links

--- bin/npm-install-plugins.sh
+++ bin/npm-install-plugins.sh
@@ -12,9 +12,9 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd)"
 DIR="$1"
 NPROC=`nproc`
 
-find "${DIR}" -name "node_modules" -print0 |
+find -L "${DIR}" -name "node_modules" -print0 |
     xargs -0 --max-procs=${NPROC} rm -Rf
 
-find "${DIR}" -name "package.json" -print0 |
+find -L "${DIR}" -name "package.json" -print0 |
     sed s,/package.json,,g |
     xargs -0 --max-procs=${NPROC} -I % bash -c "${SCRIPT_DIR}/npm-install.sh %"
